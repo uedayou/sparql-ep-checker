1e617914b0a475474103c2d150185ede123268c7
uedayou <yooueda@gmail.com>
2021-07-03 14:59:23 +0900

    modify

--- src/index.js
+++ src/index.js
@@ -11,28 +11,40 @@ async function main() {
 
 	for (const obj of urllist) {
 		let url = obj.url;
-		let param;
-		for (const key in obj.params) {
-			param = param ? param+"&" : "?";
-			param += key+"="+encodeURI(obj.params[key]);
-		}
-		if (param) url += param;
-		const flag = await checkUrl(url);
+		const flag = await checkUrl(url, obj.params, obj.type);
+		console.log(`check : ${obj.url} => ${(flag ? "〇" : "×")}`);
 		const svg = flag ? success : failure;
-		const dir = docs_dir+encodeURIComponent(obj.url)+"/";
+		const dir_name = obj.name || encodeURIComponent(obj.url);
+		const dir = docs_dir+dir_name+"/";
 		!fs.existsSync(dir) && fs.mkdirSync(dir, { recursive: true });
 		fs.writeFileSync(dir+"check.svg", svg);
+		fs.writeFileSync(dir+"update.txt", new Date().toString());
 	}
 	fs.writeFileSync(docs_dir+"update.txt", new Date().toString());
 }
 
-async function checkUrl(url) {
+async function checkUrl(url, params, type) {
+	params = params || [];
+	type = type || "GET";
+	//console.log(url);
 	let flag = false;
 	try {
-		const res = await axios(url);
+		let res;
+		if (type.match(/get/i)) {
+			res = await axios.get(url, {
+				params
+			});
+		} else {
+			res = await axios.post(url, {
+				...params
+			}, {
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			});
+		}
 		flag = true;
 	} catch (e) {
-		console.error(e);
+		//console.error(e);
+		console.error(e.response ? e.response.status : e.errno);
 	}
 	return flag;
 }
